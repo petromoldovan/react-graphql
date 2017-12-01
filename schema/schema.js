4dcdae539403ed6d733b302c506b4e4aad5d4ae9
petromoldovan <petro.moldovan@dcso.de>
2017-12-01 11:17:43 +0100

    mutations

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,8 @@ const {
 	GraphQLString,
 	GraphQLInt,
 	GraphQLSchema,
-	GraphQLList
+	GraphQLList,
+	GraphQLNonNull
 } = graphql
 
 const CompanyType = new GraphQLObjectType({
@@ -73,6 +74,51 @@ const RootQuery = new GraphQLObjectType({
 	}
 })
 
+
+const RootMutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addUser: {
+			//type is here type of data that resolve func will return
+			type: UserType,
+			//args here is the stuff that will be provided by the query. Sort of body object
+			args: {
+				firstName: {type: new GraphQLNonNull(GraphQLString)},
+				age: {type: new GraphQLNonNull(GraphQLInt)},
+				companyId: {type: GraphQLString},
+			},
+			resolve(parentValue, {firstName, age}) {
+				return axios.post(`http://localhost:3000/users`, {firstName, age})
+					.then(resp => resp.data) //trick for axios
+			}
+		},
+		deleteUser: {
+			type: UserType,
+			args: {
+				id: {type: new GraphQLNonNull(GraphQLString)}
+			},
+			resolve(parentValue, args) {
+				return axios.delete(`http://localhost:3000/users/${args.id}`)
+					.then(resp => resp.data) //trick for axios
+			}
+		},
+		editUser: {
+			type: UserType,
+			args: {
+				id: {type: new GraphQLNonNull(GraphQLString)},
+				firstName: {type: GraphQLString},
+				age: {type: GraphQLInt},
+				companyId: {type: GraphQLString},
+			},
+			resolve(parentValue, {id, firstName, age, companyId}) {
+				return axios.patch(`http://localhost:3000/users/${id}`, {firstName, age, companyId})
+					.then(resp => resp.data) //trick for axios
+			}
+		}
+	}
+})
+
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation: RootMutation
 })
